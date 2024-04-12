3689523a362d398e7edbde4e8c197f5d9226d4aa
Faisal Shahzad <faisal.shahzad@synavos.com>
2024-04-13 02:07:52 +0500

    added framework

---
+++ FSNetworkManager/FSNetworkManager/FSNetworkManager.h
@@ -0,0 +1,18 @@
+//
+//  FSNetworkManager.h
+//  FSNetworkManager
+//
+//  Created by Faisal Shahzad on 13/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FSNetworkManager.
+FOUNDATION_EXPORT double FSNetworkManagerVersionNumber;
+
+//! Project version string for FSNetworkManager.
+FOUNDATION_EXPORT const unsigned char FSNetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSNetworkManager/PublicHeader.h>
+
+
---
+++ FSNetworkManager/FSNetworkManager/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  FSNetworkManager
+//
+//  Created by Faisal Shahzad on 13/04/2024.
+//
+
+import Foundation
+
+public struct NetworkManager {
+
+    public static func test() {
+        print("it is a frameword test")
+    }
+    
+    
+}
